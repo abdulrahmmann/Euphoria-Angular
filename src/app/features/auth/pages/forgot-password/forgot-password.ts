28be26ffc096bc0df835211389778cb0e396ac81
Abdulrahman <abd3lra7man.078@gmail.com>
2025-10-30 19:55:47 +0300

    add a toasts.

--- src/app/features/auth/pages/forgot-password/forgot-password.ts
+++ src/app/features/auth/pages/forgot-password/forgot-password.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {InputText} from "primeng/inputtext";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {RouterLink} from '@angular/router';
+import {Toast} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,18 +11,43 @@ import {RouterLink} from '@angular/router';
     InputText,
     ReactiveFormsModule,
     RouterLink,
+    Toast,
   ],
+  providers: [MessageService],
   templateUrl: './forgot-password.html',
   styles: ``,
 })
 export class ForgotPassword {
+  private messageService = inject(MessageService);
+
   resetPasswordForm = new FormGroup({
     resetEmail: new FormControl(),
   });
 
-  onSubmit() {
-    const email: string = this.resetPasswordForm.controls.resetEmail.value;
+  get emailControl() {
+    return this.resetPasswordForm.controls.resetEmail;
+  }
 
-    console.log("Email: ", email);
+  onSubmit() {
+    if (this.emailControl.invalid) {
+      this.emailControl.markAsTouched();
+
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Invalid Email',
+        detail: this.emailControl.hasError('required')
+          ? 'Email is required.'
+          : 'Please enter a valid email address.',
+        life: 3000,
+      });
+      return;
+    }
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Welcome',
+      detail: 'Login successful!',
+      life: 3000,
+    });
   }
 }
--- src/app/features/auth/pages/signup/signup.ts
+++ src/app/features/auth/pages/signup/signup.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Checkbox} from 'primeng/checkbox';
 import {InputText} from 'primeng/inputtext';
 import {Password} from 'primeng/password';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 import {Divider} from 'primeng/divider';
+import { Toast } from 'primeng/toast';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-signup',
@@ -14,22 +16,80 @@ import {Divider} from 'primeng/divider';
     Password,
     ReactiveFormsModule,
     RouterLink,
-    Divider
+    Divider,
+    Toast
   ],
+  providers: [MessageService],
   templateUrl: './signup.html',
   styles: ``,
 })
 export class Signup {
+  private messageService = inject(MessageService);
+
   signupForm = new FormGroup({
-    signupEmail: new FormControl(),
-    signupPassword: new FormControl(),
+    signupEmail: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    signupPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8), Validators.maxLength(60)],
+      nonNullable: true,
+    }),
   });
 
-  onSubmit() {
-    const email: string = this.signupForm.controls.signupEmail.value;
-    const password: string = this.signupForm.controls.signupPassword.value;
+  get emailControl() {
+    return this.signupForm.controls.signupEmail;
+  }
 
-    console.log("Email: ", email);
-    console.log("Password: ", password);
+  get passwordControl() {
+    return this.signupForm.controls.signupPassword;
+  }
+
+  onSubmit() {
+    // check email
+    if (this.emailControl.invalid) {
+      this.emailControl.markAsTouched();
+
+      // add a toast for email detail error
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Invalid Email',
+        detail: this.emailControl.hasError('required')
+          ? 'Email is required.'
+          : 'Please enter a valid email address.',
+        life: 3000,
+      });
+      return;
+    }
+
+    // check password
+    if (this.passwordControl.invalid) {
+      this.passwordControl.markAsTouched();
+      let detail = 'Password is invalid.';
+
+      if (this.passwordControl.hasError('required')) {
+        detail = 'Password is required.';
+      } else if (this.passwordControl.hasError('minLength')) {
+        detail = 'Password must be at least 8 characters.';
+      } else if (this.passwordControl.hasError('maxlength')) {
+        detail = 'Password must not exceed 60 characters.';
+      }
+
+      // add a toast for password detail error
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Invalid Password',
+        detail,
+        life: 3000,
+      });
+      return;
+    }
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Welcome',
+      detail: 'Login successful!',
+      life: 3000,
+    });
   }
 }
